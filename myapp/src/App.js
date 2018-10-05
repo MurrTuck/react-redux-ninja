b225b1a323e37d4bf53ccf4aec2cb1cb7f754c49
MurrTuck <murrtuck@gmail.com>
2018-10-05 15:27:25 -0600

    Mapping

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 import Ninjas from "./Ninjas";
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Benny", age: 30, belt: "black", id: 1 },
+      { name: "Bug", age: 33, belt: "pink", id: 2 },
+      { name: "Noly", age: 27, belt: "brown", id: 3 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>My React App!</h1>
         <p>Welcome!</p>
-        <Ninjas name="Benny" age="30" belt="Green" />
-        <Ninjas name="Bug" age="33" belt="Pink" />
-        <Ninjas name="Noly" age="27" belt="Brown" />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
