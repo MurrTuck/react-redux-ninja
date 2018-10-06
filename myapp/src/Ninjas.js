915c80da7e7aaed12f8e1c4c36e9e405f41324d4
MurrTuck <murrtuck@gmail.com>
2018-10-06 17:26:21 -0600

    Conditional Output

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,16 +1,41 @@
 import React from "react";
 
 const Ninjas = ({ ninjas }) => {
-  const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div>
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
-      </div>
-    );
-  });
-  return <div className="ninja-list">{ninjaList}</div>;
+  // const ninjaList = ninjas.map(ninja => {
+  //   if (ninja.age > 29) {
+  //     return (
+  //       <div className="ninja" key={ninja.id}>
+  //         <div>Name: {ninja.name}</div>
+  //         <div>Age: {ninja.age}</div>
+  //         <div>Belt: {ninja.belt}</div>
+  //       </div>
+  //     );
+  //   } else {
+  //     return null;
+  //   }
+  // });
+  // const ninjaList = ninjas.map(ninja => {
+  //   return ninja.age > 29 ? (
+  //     <div className="ninja" key={ninja.id}>
+  //       <div>Name: {ninja.name}</div>
+  //       <div>Age: {ninja.age}</div>
+  //       <div>Belt: {ninja.belt}</div>{" "}
+  //     </div>
+  //   ) : null;
+  // });
+  return (
+    <div className="ninja-list">
+      {ninjas.map(ninja => {
+        return ninja.age > 29 ? (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default Ninjas;
